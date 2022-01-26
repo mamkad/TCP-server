456e27ad2f9ffd11a2f3a325f9b858fd9a6cf41a
mamkad <89218729+mamkad@users.noreply.github.com>
2022-01-26 15:16:08 +0300

    create of server

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	server main_server();
-
+	function<uint16_t(uint16_t)> encrypt_func = [](uint16_t i) {return 2 * (2 * (i + 1) + 1); };
+	
+	server main_server("0.0.0.0", "8000", "8001", 200, "log.txt", encrypt_func);
 	return main_server.execute();
 }
