45f3f7d6f115f982dd48c743a6bbc8526f84aafa
mamkad <89218729+mamkad@users.noreply.github.com>
2022-01-27 16:45:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include "server.hpp"
 
-int main()
+#define COUNTOFARGS 6
+
+// "0.0.0.0", "8000", "8001", 200, "log.txt"
+
+int main(int argc, char** argv)
 {
-	function<uint16_t(uint16_t)> encrypt_func = [](uint16_t i) {return 2 * (2 * (i + 1) + 1); };
+	if (argc != COUNTOFARGS) {
+		cout << "wrong number of argument" << '\n';
+		return 0;
+	}
+
+	try {
+		function<uint16_t(uint16_t)> encrypt_func = [](uint16_t i) {return 2 * (2 * (i + 1) + 1); };
 	
-	server main_server("0.0.0.0", "8000", "8001", 200, "log.txt", encrypt_func);
-	return main_server.execute();
+		server main_server(argv[1], argv[2], argv[3], std::stoi(argv[4]), argv[5], encrypt_func);
+
+		return main_server.execute();
+
+	} catch(exception const& e) {
+		cout << e.what() << '\n';
+	}
+
+	return 0;
 }
